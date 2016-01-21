5d02287f5140172da933a5291f96210fd72b105a
Sunny <me@darkowlzz.space>
2016-01-21 17:23:05 +0530

    Added text style and size. Removed extra layout view using merge.

--- minimaldivider/src/main/java/space/darkowlzz/minimaldivider/MinimalDivider.java
+++ minimaldivider/src/main/java/space/darkowlzz/minimaldivider/MinimalDivider.java
@@ -5,29 +5,30 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-/**
- * Created by sunny on 20/01/16.
- */
 public class MinimalDivider extends LinearLayout {
 
-    private final int DEFAULT_DIVIDERLINE_WIDTH = 197;
-    private final int DEFAULT_DIVIDERLINE_HEIGHT = 5;
-    private final int DEFAULT_DIVIDERLINE_TOP_MARGIN = 26;
-    private final int DEFAULT_DIVIDERLINE_BOTTOM_MARGIN = 26;
+    private final int DEFAULT_TEXT_STYLE = 0;
+    private final int DEFAULT_TEXT_SIZE = 0;
+    private final int DEFAULT_TEXT_COLOR = 0;
 
     private TextView topTextView, bottomTextView;
     private View dividerView;
 
     private boolean topTextEnabled = false;
     private String topTextValue = "";
+    private int topTextStyle;
+    private float topTextSize;
+    private int topTextColor;
     private boolean bottomTextEnabled = false;
     private String bottomTextValue = "";
+    private int bottomTextStyle;
+    private float bottomTextSize;
+    private int bottomTextColor;
 
     private int dividerLineColor;
     private int dividerLineWidth;
@@ -56,6 +57,10 @@ public class MinimalDivider extends LinearLayout {
         initViews(context, attrs);
     }
 
+    private int getDimensionResource(int res) {
+        return getResources().getDimensionPixelSize(res);
+    }
+
     private void initViews(final Context context, AttributeSet attrs) {
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.MinimalDivider, 0, 0);
 
@@ -66,10 +71,23 @@ public class MinimalDivider extends LinearLayout {
             topTextValue = a.getString(R.styleable.MinimalDivider_topText_value);
             bottomTextValue = a.getString(R.styleable.MinimalDivider_bottomText_value);
 
-            dividerLineWidth = a.getDimensionPixelSize(R.styleable.MinimalDivider_dividerLine_width, DEFAULT_DIVIDERLINE_WIDTH);
-            dividerLineHeight = a.getDimensionPixelSize(R.styleable.MinimalDivider_dividerLine_height, DEFAULT_DIVIDERLINE_HEIGHT);
-            dividerLineMarginTop = a.getDimensionPixelSize(R.styleable.MinimalDivider_dividerLine_marginTop, DEFAULT_DIVIDERLINE_TOP_MARGIN);
-            dividerLineMarginBottom = a.getDimensionPixelSize(R.styleable.MinimalDivider_dividerLine_marginBottom, DEFAULT_DIVIDERLINE_BOTTOM_MARGIN);
+            topTextStyle = a.getResourceId(R.styleable.MinimalDivider_topText_style, DEFAULT_TEXT_STYLE);
+            bottomTextStyle = a.getResourceId(R.styleable.MinimalDivider_bottomText_style, DEFAULT_TEXT_STYLE);
+
+            topTextSize = a.getDimensionPixelSize(R.styleable.MinimalDivider_topText_size, DEFAULT_TEXT_SIZE);
+            bottomTextSize = a.getDimensionPixelSize(R.styleable.MinimalDivider_bottomText_size, DEFAULT_TEXT_SIZE);
+
+            topTextColor = a.getColor(R.styleable.MinimalDivider_topText_color, DEFAULT_TEXT_COLOR);
+            bottomTextColor = a.getColor(R.styleable.MinimalDivider_bottomText_color, DEFAULT_TEXT_COLOR);
+
+            dividerLineWidth = a.getDimensionPixelSize(R.styleable.MinimalDivider_dividerLine_width,
+                                                getDimensionResource(R.dimen.default_divider_width));
+            dividerLineHeight = a.getDimensionPixelSize(R.styleable.MinimalDivider_dividerLine_height,
+                                                getDimensionResource(R.dimen.default_divider_height));
+            dividerLineMarginTop = a.getDimensionPixelSize(R.styleable.MinimalDivider_dividerLine_marginTop,
+                                                getDimensionResource(R.dimen.default_divider_marginTop));
+            dividerLineMarginBottom = a.getDimensionPixelSize(R.styleable.MinimalDivider_dividerLine_marginBottom,
+                                                getDimensionResource(R.dimen.default_divider_marginBottom));
 
             if(!isInEditMode()) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -87,24 +105,54 @@ public class MinimalDivider extends LinearLayout {
 
         LayoutInflater.from(context).inflate(R.layout.minimaldivider_layout, this);
 
+        setOrientation(VERTICAL);
+
         topTextView = (TextView) findViewById(R.id.topText);
         dividerView = findViewById(R.id.divider);
         bottomTextView = (TextView) findViewById(R.id.bottomText);
 
+        // Set text content
         topTextView.setText(topTextValue);
-        topTextView.setVisibility(topTextEnabled ? VISIBLE : GONE);
-
         bottomTextView.setText(bottomTextValue);
+
+        // Set text visibility
+        topTextView.setVisibility(topTextEnabled ? VISIBLE : GONE);
         bottomTextView.setVisibility(bottomTextEnabled ? VISIBLE : GONE);
 
+        // Set text appearance style
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            topTextView.setTextAppearance(topTextStyle);
+            bottomTextView.setTextAppearance(bottomTextStyle);
+        } else {
+            topTextView.setTextAppearance(context, topTextStyle);
+            bottomTextView.setTextAppearance(context, bottomTextStyle);
+        }
+
+        // Set text size
+        if (topTextSize != DEFAULT_TEXT_SIZE) {
+            topTextView.setTextSize(topTextSize);
+        }
+        if (bottomTextSize != DEFAULT_TEXT_SIZE) {
+            bottomTextView.setTextSize(bottomTextSize);
+        }
+
+        // Set text color
+        if (topTextColor != DEFAULT_TEXT_COLOR) {
+            topTextView.setTextColor(topTextColor);
+        }
+        if (bottomTextColor != DEFAULT_TEXT_COLOR) {
+            bottomTextView.setTextColor(bottomTextColor);
+        }
+
+        // Set divider line color
         dividerView.setBackgroundColor(dividerLineColor);
 
+        // Set divider dimensions and margins
         LayoutParams params = (LayoutParams) dividerView.getLayoutParams();
         params.width = dividerLineWidth;
         params.height = dividerLineHeight;
         params.topMargin = dividerLineMarginTop;
         params.bottomMargin = dividerLineMarginBottom;
-
         dividerView.setLayoutParams(params);
     }
 }
